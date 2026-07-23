4e3fa4899fd849b02fa11d9250f9a0c8df4f9e51
rickaym <psmtitant@gmail.com>
2026-07-23 14:45:31 +0000

    feat: enable rust_analyzer with clippy diagnostics

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls" }
+local servers = { "html", "cssls", "ts_ls", "rust_analyzer" }
+
+-- Use clippy as the linter for Rust so diagnostics come from cargo clippy.
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      check = { command = "clippy" },
+      checkOnSave = true,
+    },
+  },
+})
+
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
